28f971f536f79642a9c6e4f13c9c48c1e8aca71f
Waseem <wyameen152@gmail.com>
2024-04-27 16:43:52 +0500

    Bug Fixes

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoute");
+const classRoutes = require("./routes/classRoute");
+const quizRoutes = require("./routes/quizRoute");
+const questionRoutes = require("./routes/questionRoute");
 require("./utils/db");
 
 const app = express();
@@ -11,6 +14,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", userRoutes);
+app.use("/api", classRoutes);
+app.use("/api", quizRoutes);
+app.use("/api", questionRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,28 @@
 import { Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import Authentication from "./Routes/Authentication/authentication.jsx";
-
+import Home from "./Routes/Home/home.jsx";
+import Teacher from "./Routes/Teacher/teacher.jsx";
+import Student from "./Routes/Student/student.jsx";
+import ClassQuiz from "./Components/StudentClass/classQuiz.jsx";
+import NavigationBar from "./Components/NavigationBar/navigationBar.jsx";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
-    <Routes>
-      <Route path="auth" element={<Authentication />}></Route>
-    </Routes>
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<NavigationBar />}>
+          <Route index element={<Home />}></Route>
+          <Route path="auth" element={<Authentication />}></Route>
+          <Route path="teacher" element={<Teacher />}></Route>
+          <Route path="student" element={<Student />}></Route>
+          <Route path="student/:id" element={<ClassQuiz />} />
+          <Route path="*" element={<h1>Not Found</h1>}></Route>
+        </Route>
+      </Routes>
+    </>
   );
 }
 
